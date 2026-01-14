3a16a899129a8211a2de2d2e8ebf2c5480f2f70e
JayveeFortissimo <jayveemaniquiz0123@gmail.com>
2026-01-15 03:55:23 +0800

    feat ad hero banner at Dynamic route

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="min-h-[3rem] border-t-gray-600 flex justify-center items-center text-center text-gray-600 text-sm">
+    <footer className="min-h-[5rem] border-t-gray-600 flex justify-center items-center text-center text-gray-600 text-sm">
         <p>@Jayvee Maniquiz XD</p>
     </footer>
   )
--- src/page/ViewMore.tsx
+++ src/page/ViewMore.tsx
@@ -4,12 +4,19 @@ import type { RootState } from "@/store/storeMain";
 import { useEffect } from "react";
 import { fetchBlogs } from "@/store/blogs";
 import { DynamicSkeletone } from "@/components/common/SkeletonLoading";
+import { HeroSection } from "@/components/sections/Hero";
+import { useNavigate } from "react-router-dom";
+import violin from "@/assets/Image/Violin.jpg";
+
 
 const ViewMore = () => {
   const dispatch = useDispatch();
+  const router = useNavigate();
 
   const { blogId } = useParams<{ blogId: string }>();
-  const { blogs, isLoading } = useSelector((state: RootState) => state.createBlog);
+  const { blogs, isLoading } = useSelector(
+    (state: RootState) => state.createBlog
+  );
 
   useEffect(() => {
     dispatch(fetchBlogs() as never);
@@ -19,28 +26,50 @@ const ViewMore = () => {
   const findBlog = blogs.find((blog) => blog.blog_id === blogIdNumber);
 
   return (
-    <div className="min-h-screen container mx-auto p-2 flex justify-center">
-    {
-      isLoading ? (
-        <DynamicSkeletone />
-      ) : (
-         <div className="w-full flex flex-col items-center">
-       <div className="rounded-lg overflow-hidden w-full lg:h-[40rem] mb-4">
-        <img src={findBlog?.blog_img} alt="" className="w-full object-cover"/>
-       </div>
+    <div>
+      <div className="min-h-screen container mx-auto p-2 flex justify-center ">
+        {isLoading ? (
+          <DynamicSkeletone />
+        ) : (
+          <div className="w-full flex flex-col items-center">
+            <div className="rounded-lg overflow-hidden w-full lg:h-[40rem] mb-4">
+              <img
+                src={findBlog?.blog_img}
+                alt=""
+                className="w-full object-cover"
+              />
+            </div>
 
-      <div className="py-5 w-full">
-        <h2 className="text-xl  md:text-2xl lg:text-4xl font-semibold mb-2">{findBlog?.blog_title}</h2>
-        {findBlog?.blog_subtitle && (
-          <p className="text-gray-600 mb-2 text-lg  md:text-xl lg:text-2xl ">{findBlog?.blog_subtitle}</p>
+            <div className="py-5 w-full">
+              <h2 className="text-xl  md:text-2xl lg:text-4xl font-semibold mb-2">
+                {findBlog?.blog_title}
+              </h2>
+              {findBlog?.blog_subtitle && (
+                <p className="text-gray-600 mb-2 text-lg  md:text-xl lg:text-2xl ">
+                  {findBlog?.blog_subtitle}
+                </p>
+              )}
+              <p className="text-gray-700 line-clamp-1 break-word">
+                {findBlog?.blog_description}
+              </p>
+            </div>
+          </div>
         )}
-        <p className="text-gray-700 line-clamp-1 break-word">
-          {findBlog?.blog_description}
-        </p>
       </div>
-     </div>
-      )
-    }
+
+      <header className="w-full mt-5">
+        <HeroSection
+          variant="outlined"
+          backgroundImage={violin}
+          title={"CREATE AND POST A BLOG NOW"}
+          description={
+            "We encourage all of the musicians around the world that use this platform to motivate young musicians"
+          }
+          className="min-h-[26.25rem]! mb-5 bg-[#111820]"
+          button={"Create Account Now!"}
+          onClick={() => router("/register")}
+        />
+      </header>
     </div>
   );
 };
